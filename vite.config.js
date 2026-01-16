b943890682f57cf36da78916a6aebe910049b3c8
Albert Jannsen Ramos <2221023@slu.edu.ph>
2026-01-16 14:25:45 +0800

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { serverConfig } from './vite.server.config.js'
+import { serverConfig } from './frontend/vite.server.config.js'
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/Automated-Legal-Knowledge-Base",
   server: serverConfig
 })
