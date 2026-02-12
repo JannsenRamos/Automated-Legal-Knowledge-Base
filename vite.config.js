8d6e887a8f589ac58622917f0609489777a7d6cb
Albert Jannsen Ramos <2221023@slu.edu.ph>
2026-02-12 22:00:50 +0800

    removed vercel

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { serverConfig } from './frontend/vite.server.config.js'
-
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Automated-Legal-Knowledge-Base",
-  server: serverConfig
-})
